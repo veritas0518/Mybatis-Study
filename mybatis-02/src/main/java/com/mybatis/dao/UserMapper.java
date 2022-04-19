9b859847abaf742cabb7479a663f270a9a491e76
xiaoXue <1450985307@qq.com>
2022-04-19 15:12:33 +0800

    UserMapper.java

---
+++ mybatis-02/src/main/java/com/mybatis/dao/UserMapper.java
@@ -0,0 +1,32 @@
+package com.mybatis.dao;
+
+import com.mybatis.pojo.User;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserMapper {
+
+    //模糊查询
+    List<User> getUserLike(String value);
+
+    //查询全部用户
+    List<User> getUserList();
+
+    //根据id查询用户
+    User getUserById(int id);
+
+    User getUserById2(Map<String,Object> map);
+
+    //insert一个用户
+    int addUser(User user);
+
+    //万能的Map
+    int addUser2(Map<String,Object> map);
+
+    //修改用户
+    int updateUser(User user);
+
+    //删除一个用户
+    int deleteUser(int id);
+}
