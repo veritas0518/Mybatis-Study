261b9ab96695c7166b525b3897277a274003d808
xiaoXue <1450985307@qq.com>
2022-04-25 18:30:40 +0800

    User.java

---
+++ mybatis-03/src/main/java/com/mybatis/pojo/User.java
@@ -0,0 +1,58 @@
+package com.mybatis.pojo;
+
+import org.apache.ibatis.type.Alias;
+
+/**
+ * @ClassName: User
+ * @Description:
+ * @Author: TianXing.Xue
+ * @Date: 2021/10/10 18:24
+ **/
+
+public class User {
+    private int id;
+    private String NAME;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String NAME, String password) {
+        this.id = id;
+        this.NAME = NAME;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", NAME='" + NAME + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
